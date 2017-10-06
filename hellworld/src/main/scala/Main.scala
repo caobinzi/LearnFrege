7faececbb2055baf98f1e604464b0f8fb9888e54
Binzi Cao <binzi.cao@bt.com>
2017-10-06 14:35:01 +1100

    Frege more

---
+++ hellworld/src/main/scala/Main.scala
@@ -0,0 +1,10 @@
+import example.FregeWeb.TRequest
+import example.FregeWeb.TResponse
+import example.Json
+
+import frege.run8.Box
+object ScalaMain extends App {
+
+  println(Json.testJson.call)
+
+}
